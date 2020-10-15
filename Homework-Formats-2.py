c3392e40fdcb1213e33a451fe21325da8bfd45a2
Евгений Мызников <evgenym@MacBook-Pro-Evgenij.local>
2020-10-15 15:44:16 +0300

    no changes

--- Homework-Formats-2.py
+++ Homework-Formats-2.py
@@ -17,7 +17,7 @@ def read_file_to_top_10(file_name):
                     top_list.append(word)
                     top_word_list = Counter(top_list)
                     top_10_list = sorted(top_word_list.items(), key=lambda x: x[1], reverse=True)
-        return top_10_list[0:10]
+    return top_10_list[0:10]
 
 
 print(read_file_to_top_10('newsafr.xml'))
